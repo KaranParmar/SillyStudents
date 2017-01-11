4ee7fcaa27c31cfc82d7d08d4ffae78c249ec27d
Karan Parmar <karandipparmar@hotmail.com>
2017-01-10 20:19:56 -0800

    Final commit

--- Student.php
+++ Student.php
@@ -1,46 +1,43 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Student
+ * This class includes description (name, surname, email, grades) of students 
+ * with functions to add in new emails and grades. There is also a function to
+ * calculate the average grade and toString to make it look readable.
  *
  * @author karan
  */
 class Student {
-    
+
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
-    
-    function add_email($which,$address) {
+
+    function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
 
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
-    
+
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
             $total += $value;
         return $total / count($this->grades);
     }
-    
+
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
-        $result .= ' ('.$this->average().")\n";
-        foreach($this->emails as $which=>$what)
-            $result .= $which . ': '. $what. "\n";
+        $result .= ' (' . $this->average() . ")\n";
+        foreach ($this->emails as $which => $what)
+            $result .= $which . ': ' . $what . "\n";
         $result .= "\n";
-        return '<pre>'.$result.'</pre>';
+        return '<pre>' . $result . '</pre>';
     }
+
 }
